3920941aff5c53527ccdac7948cde898f947f5e2
unknown <deanshak9@gmail.com>
2020-12-12 23:38:49 +0200

    table added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import StockRow from './components/StockRow'
 
 function App() {
   return (
-    <div className="main-section">Hello World</div>
+    <div className="App">
+      <div className="container">
+        <table className="table mt-5">
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <StockRow ticker="appl"/>
+            <StockRow ticker="goog"/>
+            <StockRow ticker="msft"/>
+            <StockRow ticker="tsla"/>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
