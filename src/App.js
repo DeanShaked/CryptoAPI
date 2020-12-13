498cf7e29488d04f8516ed0e3d921ed1c1af153c
unknown <deanshak9@gmail.com>
2020-12-13 19:13:56 +0200

    API fetched

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import StockRow from './components/StockRow'
+import React,{ useState, useEffect} from 'react'
+import Axios from 'axios';
+import Coin from './components/Coin'
 
 function App() {
+
+  const [coins,setCoins] = useState([]);
+  const [search,setSearch] = useState('');
+
+  useEffect(() => {
+    Axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+
+    }).catch(error => alert('wrong one'))
+    
+  })
+  const handleChange = e => {
+    setSearch(e.targer.value)
+  }
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
   return (
-    <div className="App">
-      <div className="container">
-        <table className="table mt-5">
-          <thead>
-            <tr>
-              <th>Ticker</th>
-              <th>Price</th>
-              <th>Date</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <StockRow ticker="appl"/>
-            <StockRow ticker="goog"/>
-            <StockRow ticker="msft"/>
-            <StockRow ticker="tsla"/>
-          </tbody>
-        </table>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a Currency</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input" onChange={handleChange}/>
+        </form>
       </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol} 
+          volume={coin.market_cup}
+          price={coin.current_price} />
+        )
+      })}
     </div>
   );
 }
